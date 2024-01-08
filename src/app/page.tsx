fb59fab04ea121cebded5446e8f5e62493a8675c
Eliot Lim <eliot.lim@gmail.com>
2024-01-08 22:34:25 +0800

    feat: basic brand search
    + working npm, pypi, crates, github search
    + basic ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,52 @@
 import BrandSearch from "@/components/brandsearch";
+import z from "zod";
 
 export default function Home() {
+  async function search(data: FormData) : Promise<{result: Record<string, string>} | {error: Record<string, string>}> {
+    'use server';
+
+    const brandInput = z.string().min(1).max(100);
+    const parseBrand = brandInput.safeParse(data.get('brand'));
+    if (!parseBrand.success) {
+      return {
+        error: {
+          message: 'Invalid brand name',
+          zod: parseBrand.error.toString(),
+        },
+      }
+    }
+
+    console.log(`Querying for: ${parseBrand.data}...`);
+
+    const fetchConfig = {headers: {'User-Agent': 'brand-kit-search'}};
+
+    // check github
+    const github = (await fetch(`https://www.github.com/${parseBrand.data}`, fetchConfig)).status === 404;
+
+    // check npm
+    const npm = (await fetch(`https://www.npmjs.com/package/${parseBrand.data}`, fetchConfig)).status === 404;
+
+    // check pypi
+    const pypi = (await fetch(`https://pypi.org/project/${parseBrand.data}`, fetchConfig)).status === 404;
+
+    // check crates
+    const crates = (await fetch(`https://crates.io/api/v1/crates/${parseBrand.data}`, fetchConfig)).status === 404;
+
+    console.log(`Github: ${github}, NPM: ${npm}, PyPI: ${pypi}, Crates: ${crates}`)
+
+    return {
+      result: {
+        github: github ? '✅ Available' : '❌ Taken',
+        npm: npm ? '✅ Available' : '❌ Taken',
+        pypi: pypi ? '✅ Available' : '❌ Taken',
+        crates: crates ? '✅ Available' : '❌ Taken',
+      },
+    };
+  }
+
   return (
     <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <BrandSearch/>
+      <BrandSearch search={search}/>
     </main>
   )
 }
--- src/components/brandsearch.tsx
+++ src/components/brandsearch.tsx
@@ -1,33 +1,50 @@
-export default function BrandSearch() {
+'use client';
+import {useCallback, useState} from "react";
 
-  async function search() {
-    'use server';
-  }
+interface BrandSearchProps{
+  search: (data: FormData) => Promise<{result: Record<string, string>} | {error: Record<string, string>}>;
+}
+export default function BrandSearch(props: BrandSearchProps) {
+
+  const [result, setResult] = useState<Record<string, string>>({});
+
+  const onSubmit = useCallback(async (event: FormData) => {
+    const response = await props.search(event);
+    if ('error' in response) {
+      console.error(response.error);
+      setResult(response.error);
+      return;
+    }
+
+    setResult(response.result);
+  }, [props]);
 
   return (
     <div className="text-center">
       <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Brand Kit</h1>
       <p className="mt-6 text-base leading-7 text-gray-600">Search everywhere* for your next brand</p>
-      <form action={'hi'}>
+      <form action={onSubmit}>
         <div className="mt-10 flex items-center justify-center gap-x-6">
-
           <input
             className="rounded-md bg-gray-50 px-3.5 py-2.5 text-sm font-normal text-black shadow-sm hover:bg-gray-100 outline outline-2 outline-offset-2 outline-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             type="text"
+            name="brand"
             placeholder={"Search for a brand"}
           >
           </input>
-          <a
-            href="#"
+          <input
+            type="submit"
             className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            value={"Search"}
           >
-            Search
-          </a>
+          </input>
+        </div>
+        <div className="mt-10 flex items-center justify-center gap-x-6 text-gray-900">
+          <ul>
+            {result ? Object.keys(result).map(entry => <><p className="text-gray-900">{entry}: {result[entry]}</p></>) : "No results yet"}
+          </ul>
         </div>
       </form>
-      <div className="mt-10 flex items-center justify-center gap-x-6">
-
-      </div>
     </div>
   )
 }
