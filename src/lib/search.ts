6ad1f5f5e469a3cac30fedf0c39ed81465fa8abe
Eliot Lim <eliot.lim@gmail.com>
2024-01-24 20:27:14 +0800

    fix: search concurrently
    + use Promise.allSettled to search concurrently
    + should be faster than a synchronous search

--- src/lib/search.ts
+++ src/lib/search.ts
@@ -2,6 +2,52 @@
 import z from "zod";
 import {SearchResponse, SearchStatus} from "@/lib/types";
 
+interface Checker {
+  namespace: string;
+  check(brand: string): Promise<boolean>;
+}
+
+const fetchConfig = {headers: {'User-Agent': 'brand-kit-search'}};
+
+class GithubChecker implements Checker {
+  namespace = 'github';
+  async check(brand: string): Promise<boolean> {
+    const response = await fetch(`https://www.github.com/${brand}`, fetchConfig);
+    return response.status === 404;
+  }
+}
+
+class NpmChecker implements Checker {
+  namespace = 'npm';
+  async check(brand: string): Promise<boolean> {
+    const response = await fetch(`https://www.npmjs.com/package/${brand}`, fetchConfig);
+    return response.status === 404;
+  }
+}
+
+class PypiChecker implements Checker {
+  namespace = 'pypi';
+  async check(brand: string): Promise<boolean> {
+    const response = await fetch(`https://pypi.org/project/${brand}`, fetchConfig);
+    return response.status === 404;
+  }
+}
+
+class CratesChecker implements Checker {
+  namespace = 'crates';
+  async check(brand: string): Promise<boolean> {
+    const response = await fetch(`https://crates.io/api/v1/crates/${brand}`, fetchConfig);
+    return response.status === 404;
+  }
+}
+
+const checkers: Checker[] = [
+  new GithubChecker(),
+  new NpmChecker(),
+  new PypiChecker(),
+  new CratesChecker(),
+];
+
 export const search = async (data: FormData): Promise<SearchResponse> => {
   const brandInput = z.string().min(1).max(100);
   const parseBrand = brandInput.safeParse(data.get('brand'));
@@ -15,30 +61,23 @@ export const search = async (data: FormData): Promise<SearchResponse> => {
 
   console.log(`Querying for: ${parseBrand.data}...`);
 
-  const fetchConfig = {headers: {'User-Agent': 'brand-kit-search'}};
-
-  // check github
-  const github = (await fetch(`https://www.github.com/${parseBrand.data}`, fetchConfig)).status === 404;
-
-  // check npm
-  const npm = (await fetch(`https://www.npmjs.com/package/${parseBrand.data}`, fetchConfig)).status === 404;
-
-  // check pypi
-  const pypi = (await fetch(`https://pypi.org/project/${parseBrand.data}`, fetchConfig)).status === 404;
-
-  // check crates
-  const crates = (await fetch(`https://crates.io/api/v1/crates/${parseBrand.data}`, fetchConfig)).status === 404;
-
-  console.log(`Github: ${github}, NPM: ${npm}, PyPI: ${pypi}, Crates: ${crates}`)
+  const all = await Promise.allSettled(checkers.map(async (checker) => {
+    return {
+      namespace: checker.namespace,
+      available: await checker.check(parseBrand.data),
+    };
+  }));
 
   return {
     success: true,
     brand: parseBrand.data,
-    results: {
-      github: github ? SearchStatus.AVAILABLE : SearchStatus.TAKEN,
-      npm: npm ? SearchStatus.AVAILABLE : SearchStatus.TAKEN,
-      pypi: pypi ? SearchStatus.AVAILABLE : SearchStatus.TAKEN,
-      crates: crates ? SearchStatus.AVAILABLE : SearchStatus.TAKEN,
-    },
+    results: all.reduce((acc, curr) => {
+      if (curr.status === 'fulfilled') {
+        acc[curr.value.namespace] = curr.value.available ? SearchStatus.AVAILABLE : SearchStatus.TAKEN;
+      } else {
+        acc[curr.reason.namespace] = SearchStatus.TAKEN;
+      }
+      return acc;
+    }, {} as Record<string, SearchStatus>),
   };
 };
