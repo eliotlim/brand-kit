c6f2a6669c56c195b6dfef8f1b3522a6c94bba34
Eliot Lim <eliot.lim@gmail.com>
2024-03-11 22:17:23 +0800

    fix: mobile results table
    + improve mobile results table
    + add overflow scroll
    + add fixed brand column

--- src/components/results-table.tsx
+++ src/components/results-table.tsx
@@ -13,11 +13,12 @@ export const ResultsTable = () => {
 
   return (
     <div className="mt-10 flex flex-col items-center justify-center gap-x-6 text-gray-900 dark:text-gray-100">
+      <div className="w-full overflow-x-scroll">
       {searchResults && searchResults.length > 0 &&
-        <table className="border-separate border-spacing-x-10 border-spacing-y-2">
+        <table className="border-separate border-spacing-x-6 border-spacing-y-2 overflow-x-scroll w-full table-auto">
           <thead>
           <tr>
-            <th>
+            <th className="sticky left-0 h-fit bg-gray-50 dark:bg-gray-800 text-left">
               <p className="text-gray-900 dark:text-gray-100">Results</p>
             </th>
             {locations.map(location => (<>
@@ -32,7 +33,7 @@ export const ResultsTable = () => {
           <tbody>
           {searchResults ? searchResults.map(entry => (<>
             <tr>
-              <td className="text-gray-900 dark:text-gray-100">
+              <td className="text-gray-900 dark:text-gray-100 sticky left-0 h-fit bg-gray-50 dark:bg-gray-800 text-left">
                 {entry.brand}
               </td>
               {
@@ -47,6 +48,7 @@ export const ResultsTable = () => {
           </tbody>
         </table>
       }
+      </div>
       {searchResults.length === 0 ? "No results yet" :
         <button
           className="mt-4 bg-indigo-600 hover:bg-indigo-500 text-white font-bold py-2 px-4 rounded"
